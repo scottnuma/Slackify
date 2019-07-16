69a63c0ba8ac71561c6fa636018881d252846fb7
scottnuma <scott.numamoto@gmail.com>
2019-07-15 22:12:58 -0700

    Always use 0.0.0.0 with gunicorn

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -4,10 +4,9 @@ import dotenv
 
 dotenv.load_dotenv()
 
-if os.environ.get("FLASK_ENV") == "development":
-    bind = "localhost:5000"
-else:
-    bind = "0.0.0.0:5000"
+bind = "0.0.0.0:5000"
+
+if os.environ.get("FLASK_ENV") != "development":
     ssl_dir = os.environ.get("SLACKIFY_SSL_DIR", "")
     keyfile = ssl_dir + "privkey.pem"
     certfile = ssl_dir + "cert.pem"
