75c6f301799388e2a0785ad2cca0ad4f9c1348e5
scottnuma <scott.numamoto@gmail.com>
2019-07-24 16:22:04 -0700

    Update testing for Travis

--- scripts/unit-test.sh
+++ scripts/unit-test.sh
@@ -8,6 +8,9 @@ docker run \
     --env VAULT_TOKEN=$VAULT_TOKEN \
     --env VAULT_ADDR=$VAULT_ADDR \
     --env PRODUCTION_URL=$PRODUCTION_URL \
+    --env GCLOUD_PROJECT=$GCLOUD_PROJECT \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/app/service_account.json \
+    --env VAULT_GCP_SERVICE_ACCOUNT_LEASE_NAME_FILE=/app/lease.txt \
     --name slackify-testing \
     slackify \
     pipenv run python -m unittest
